27fdf9775112de7c94a114ecca89e400f02d0cf7
Daniel Park <parkant@bc.edu>
2022-01-30 00:17:03 -0500

    Initial Commit

---
+++ Two-Button-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two-Button-Challenge
+//
+//  Created by Danny Park on 1/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
